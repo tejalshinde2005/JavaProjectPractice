1b9d6dc279ebc66d0af29722ee671cc7ae6fb3f9
tejalshinde2005 <shindetejal2005@gmail.com>
2025-07-16 19:10:47 +0530

    Add files via upload

---
+++ HellowWorld.java
@@ -0,0 +1,11 @@
+//Q1) Write a java program Hellow word 
+package package1;
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+	System.out.println("Hellow World");
+
+	}
+
+}
