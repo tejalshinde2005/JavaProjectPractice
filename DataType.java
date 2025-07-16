e654fe4c2f811563169f2a2a7a7dc373c1afe6b8
tejalshinde2005 <shindetejal2005@gmail.com>
2025-07-16 19:13:53 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Markes = " + (30+50));
+		//if we use + operator with numeric values then it perform addition
+		//if we use + operator with String values then it perform Cocatenation
+
+	}
+
+}
